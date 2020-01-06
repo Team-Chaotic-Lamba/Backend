39ee10899d59f94cd84e0e3f83f8fffd9961aaf8
Eli Sacks <bastlifa@gmail.com>
2020-01-06 15:57:52 -0500

    more django deploy prep
    Co-authored-by: Seth Nadu <47128498+sethnadu@users.noreply.github.com>
    Co-authored-by: John Schneider <51132892+johnschneider1@users.noreply.github.com>
    Co-authored-by: Andrew Allen <30938535+AndrewA0112@users.noreply.github.com>
    Co-authored-by: Jonathan Scott <25020946+jondscott21@users.noreply.github.com>

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [a for a in config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -89,12 +90,30 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = {}
+
+if config('ENVIRONMENT') == 'local':
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.pg'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD')
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+    }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation

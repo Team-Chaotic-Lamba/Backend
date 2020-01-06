a14069cf03cbf034d1cbe6f74c661a4c8a97f291
Eli Sacks <bastlifa@gmail.com>
2020-01-06 14:00:44 -0500

    initial setup Co-authored-by: <47128498+sethnadu@users.noreply.github.com> Co-authored-by: <51132892+johnschneider1@users.noreply.github.com> Co-authored-by: <30938535+AndrewA0112@users.noreply.github.com> Co-authored-by: <25020946+jondscott21@users.noreply.github.com>

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
+from .models import *
+admin.site.register(Room)
+admin.site.register(Player)
+
+
+# curl -X POST -H 'Authorization: Token 4d35e16842ccd0d179dce147cfd04657cec9342a' -H "Content-Type: application/json" -d '{"direction":"n"}' localhost:8000/api/adv/move/
+# curl -X POST -H "Content-Type: application/json" -d '{"username":"team_chaotic", "password":"chaospass"}' localhost:8000/api/login/
+
+
+# room_type = {
+#     'cave': 'cave_deco',
+#     'hall': 'hall_deco',
+# }
+
+# cave_deco = {
+#     'damp': 'moisture fills the air and mold grows on the walls',
+#     'hot'
+# }
+
+# hall_deco = {
+
+# }
+
+# room_items = {
+#     'none': 50,
+#     'box': 53,
+# }
